de1ddbc0f8a07abe832dedab6e2c7f46accf96b3
Carlos Hugo (Asus) <carlos@appdata.mx>
2016-05-02 19:31:01 -0500

    redirect logout

--- client/components/login/login.factory.js
+++ client/components/login/login.factory.js
@@ -18,7 +18,13 @@
 			},
 			logout: function() {
 				sessionStorage.removeItem('access_token');
-				window.location = "http://52.8.211.37/walmex.latlong.mx/#/login";
+				console.log("redirect");
+				
+				// similar behavior as an HTTP redirect
+				window.location.replace("http://52.8.211.37/walmex.latlong.mx");
+
+				// similar behavior as clicking on a link
+				window.location.href = "http://52.8.211.37/walmex.latlong.mxm";
 				
 				//$location.path("/login");
 				//$location.replace();
