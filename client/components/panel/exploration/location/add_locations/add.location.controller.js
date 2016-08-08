1c9af572d18cd2fe3c97c7e9130f3088a4370281
Mario FLores <pokazzam@gmail.com>
2016-08-07 22:08:01 -0500

    testing accessibility design

--- client/components/panel/exploration/location/add_locations/add.location.controller.js
+++ client/components/panel/exploration/location/add_locations/add.location.controller.js
@@ -229,7 +229,7 @@
 					textContent: message,
 					position: 'top right',
 					hideDelay: 2500,
-					parent: $document[0].querySelector('.m-dialog--in-location')
+					parent: $document[0].querySelector('.md-dialog-container')
 				})
 			);
 		}
