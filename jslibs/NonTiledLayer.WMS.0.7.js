e9164c32e8a8f35dcf4e6da77fb89f24d0e8f17f
Mario FLores <pokazzam@gmail.com>
2016-08-31 10:08:15 -0500

    updated leaflet libraries

--- jslibs/NonTiledLayer.WMS.0.7.js
+++ jslibs/NonTiledLayer.WMS.0.7.js
@@ -12,8 +12,14 @@ L.NonTiledLayer.WMS = L.NonTiledLayer.extend({
 		styles: '',
 		format: 'image/jpeg',
 		transparent: false,
-		srs: 'EPSG:4326'
+		srs: 'EPSG:4326',
+		info_format:'geojson'
 	},
+
+	options: {
+    crs: null,
+    uppercase: false
+  },
 
 	initialize: function (url, options) { // (String, Object)
 		this._wmsUrl = url;
@@ -37,7 +43,83 @@ L.NonTiledLayer.WMS = L.NonTiledLayer.extend({
 		this.wmsParams[projectionKey] = this.options.crs.code || map.options.crs.code;
 
 		L.NonTiledLayer.prototype.onAdd.call(this, map);
+		map.off('click', this.getFeatureInfo, this);
+		map.on('click', this.getFeatureInfo, this);
 	},
+
+	getFeatureInfo: function(evt) {
+    // Make an AJAX request to the server and hope for the best
+    var url = this.getFeatureInfoUrl(evt.latlng),
+      showResults = L.Util.bind(this.showGetFeatureInfo, this);
+    $.ajax({
+      url: url,
+      success: function(data, status, xhr) {
+        var err = typeof data === 'string' ? null : data;
+        showResults(err, evt.latlng, data);
+      },
+      error: function(xhr, status, error) {
+        //console.log(error);
+        //showResults(error);
+      }
+    });
+  },
+
+  getFeatureInfoUrl: function(latlng) {
+    // Construct a GetFeatureInfo request URL given a point
+    var point = this._map.latLngToContainerPoint(latlng, this._map.getZoom()),
+      size = this._map.getSize(),
+
+      params = {
+        request: 'GetFeatureInfo',
+        service: 'WMS',
+        srs: 'EPSG:4326',
+        styles: this.wmsParams.styles,
+        transparent: this.wmsParams.transparent,
+        version: this.wmsParams.version,
+        format: this.wmsParams.format,
+        bbox: this._map.getBounds().toBBoxString(),
+        height: size.y,
+        width: size.x,
+        layers: this.wmsParams.layers,
+        query_layers: this.wmsParams.layers,
+        info_format: this.wmsParams.info_format
+      };
+
+    params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
+    params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
+
+    var url = this._wmsUrl + L.Util.getParamString(params, this._url, true);
+    if(url.indexOf('?REQUEST')!== -1){
+      url = url.replace('?REQUEST','&REQUEST');
+    }
+
+    return url;
+  },
+
+  showGetFeatureInfo: function(err, latlng, content) {
+    if (err) {
+      console.log(err);
+      return;
+    } // do nothing if there's an error
+
+    var html = "";
+    var json = JSON.parse(content);
+    _.each(json.features, function(o){
+      _.each(o.properties.description, function(oo, i){
+        _.each(oo,function(v,n){
+          html += "<strong>"+n.trim()+"</strong>:"+v.trim()+"<br>";
+        });
+      });
+    });
+
+    // Otherwise show the content in a popup, or something.
+    L.popup({
+        maxWidth: 800
+      })
+      .setLatLng(latlng)
+      .setContent(html)
+      .openOn(this._map);
+  },
 
 	getImageUrl: function (world1, world2, width, height) {
 		var wmsParams = this.wmsParams;
